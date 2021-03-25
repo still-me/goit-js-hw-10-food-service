cd05d97da26c9e8d194435585786c487aa1b22c1
Vladyslav Fomenko <71770160+still-me@users.noreply.github.com>
2021-03-25 22:11:22 +0200

    change template markup

--- src/js/markup.js
+++ src/js/markup.js
@@ -1,16 +1,18 @@
-import cardsMaker from '../templates/card.hbs';
+// import cardsMaker from '../templates/card.hbs';
 import menu from '../menu.json'
+import cardsTemplate from '../templates/cards-list.hbs'
 
 const menuRef = document.querySelector('.js-menu');
-const cardsMarkup = createMenuCardsMarkup(menu);
-menuRef.insertAdjacentHTML('beforeend', cardsMarkup)
 
-function createMenuCardsMarkup(menu) {
-    return menu
-        .map(cardsMaker)
-        // или map (card => cardsMaker(card))
-        .join('');
-}
+// const cardsMarkup = createMenuCardsMarkup(menu);
+// menuRef.insertAdjacentHTML('beforeend', cardsMarkup)
 
+// function createMenuCardsMarkup(menu) {
+//     return menu
+//         .map(cardsMaker)
+//         // или map (card => cardsMaker(card))
+//         .join('');
+// }
 
+menuRef.innerHTML = cardsTemplate(menu)
 
